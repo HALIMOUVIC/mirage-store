66c97008254ee1b31c6dd9a28a5681978dce5629
Mirage Store <contact@miragestore.com>
2026-08-31 16:48:47 +0100

    fix(security): allow script-src-attr unsafe-inline for interactive click handlers

--- tebex-headless-server.js
+++ tebex-headless-server.js
@@ -63,7 +63,9 @@ app.use(
             directives: {
                 defaultSrc: ["'self'"],
                 scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", "https://js.tebex.io"],
+                scriptSrcAttr: ["'unsafe-inline'"],
                 styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
+                styleSrcAttr: ["'unsafe-inline'"],
                 fontSrc: ["'self'", "https://fonts.gstatic.com"],
                 imgSrc: ["'self'", "data:", "https:", "blob:"],
                 frameSrc: [
